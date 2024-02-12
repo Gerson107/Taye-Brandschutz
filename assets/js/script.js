bf2df02e595e5ca7ea785c0d7e706970185c0f31
Gerson107 <danimejia1995@hotmail.com>
2024-02-12 15:50:45 -0500

    update menu tablet and mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
+const video = document.getElementById('myVideo');
+const playPauseBtn = document.getElementById('playPauseBtn');
+
+playPauseBtn.addEventListener('click', togglePlayPause);
+
+function togglePlayPause() {
+  if (video.paused || video.ended) {
+    video.play();
+    playPauseBtn.textContent = '❚❚'; // Icono de pausa
+  } else {
+    video.pause();
+    playPauseBtn.textContent = '▶'; // Icono de reproducción
+  }
+}
+
+
 function scrollToTop() {
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
   }
+
