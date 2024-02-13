fd7112705ef08c588a60cc99afe32ef8909148b2
Gerson107 <danimejia1995@hotmail.com>
2024-02-13 11:28:43 -0500

    update html, text dinamic, h2 font-4xl, creating modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ module.exports = {
   theme: {
     container: {
         center: true,
-       
     },
-
     extend: {
+      fontFamily:{
+          'bodyFont':['Standerd', 'sans-serif']
+      },
       fontSize: {
           'base': 'clamp(1rem, 0.34vw + 0.91rem, 1.19rem)',
           'lg': 'clamp(0.78rem, 0.21vw + 0.74rem, 1.08rem)',
@@ -22,12 +23,3 @@ module.exports = {
   },
   plugins: [],
 }
-// --font-size-sm: clamp(0.5rem, 0.08vw + 0.49rem, 0.61rem);
-// --font-size-base: clamp(0.63rem, 0.13vw + 0.6rem, 0.81rem);
-// --font-size-lg: clamp(0.78rem, 0.21vw + 0.74rem, 1.08rem);
-// --font-size-xl: clamp(0.98rem, 0.33vw + 0.91rem, 1.44rem);
-// --font-size-2xl: clamp(1.22rem, 0.5vw + 1.13rem, 1.92rem);
-// --font-size-3xl: clamp(1.53rem, 0.74vw + 1.39rem, 2.57rem);
-// --font-size-4xl: clamp(1.91rem, 1.07vw + 1.71rem, 3.42rem);
-// --font-size-5xl: clamp(2.38rem, 1.54vw + 2.1rem, 4.56rem);
-// --font-size-6xl: clamp(2.98rem, 2.19vw + 2.57rem, 6.08rem);
