fa1eabf7942a21559d38ea3a9eadb9d0f4caed64
Gerson107 <danimejia1995@hotmail.com>
2024-02-14 16:19:31 -0500

    setting sizes, two slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,27 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     container: {
-        center: true,
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "4rem",
+      },
     },
     extend: {
-      fontFamily:{
-          'bodyFont':['Standerd', 'sans-serif']
+      fontFamily: {
+        bodyFont: ["Standerd", "sans-serif"],
       },
       fontSize: {
-          'base': 'clamp(1rem, 0.34vw + 0.91rem, 1.19rem)',
-          'lg': 'clamp(0.78rem, 0.21vw + 0.74rem, 1.08rem)',
-          'xl': 'clamp(0.98rem, 0.33vw + 0.91rem, 1.44rem)',
-          '2xl': 'clamp(1.22rem, 0.5vw + 1.13rem, 1.92rem)',
-          '3xl': 'clamp(1.53rem, 0.74vw + 1.39rem, 2.57rem)',
-          '4xl': 'clamp(1.91rem, 1.07vw + 1.71rem, 3.42rem)',
-          '5xl': 'clamp(2.38rem, 1.54vw + 2.1rem, 4.56rem)',
-         '6xl': 'clamp(2.98rem, 2.19vw + 2.57rem, 6.08rem)'
-			},
+        base: "clamp(1rem, 0.34vw + 0.91rem, 1.19rem)",
+        lg: "clamp(0.78rem, 0.21vw + 0.74rem, 1.08rem)",
+        xl: "clamp(0.98rem, 0.33vw + 0.91rem, 1.44rem)",
+        "2xl": "clamp(1.22rem, 0.5vw + 1.13rem, 1.92rem)",
+        "3xl": "clamp(1.53rem, 0.74vw + 1.39rem, 2.57rem)",
+        "4xl": "clamp(1.91rem, 1.07vw + 1.71rem, 3.42rem)",
+        "5xl": "clamp(2.38rem, 1.54vw + 2.1rem, 4.56rem)",
+        "6xl": "clamp(2.98rem, 2.19vw + 2.57rem, 6.08rem)",
+      },
     },
   },
   plugins: [],
-}
+};
